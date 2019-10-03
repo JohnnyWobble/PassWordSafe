849944d388e58569191d9955d200bc872f008384
Johnny Wobble <tonald.drump2.0@gmail.com>
2019-10-03 12:24:56 -0400

    documentation

--- EntryClass.py
+++ EntryClass.py
@@ -7,6 +7,16 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 class Entry:
     def __init__(self, name=None, username=None, email=None, password=None, pin=None, big_pass=None):
+        """
+        init method for Entry (duh)
+
+        :param name: str
+        :param username: str
+        :param email: str
+        :param password: str
+        :param pin: str
+        :param big_pass: str
+        """
         self.name = name
         self.username = username
         self.email = email
@@ -16,11 +26,22 @@ class Entry:
         self.decrypt_list = []
 
     def fix(self):
+        """
+        Joins data with an identifier (::~~::~~::) to separate later
+
+        :return: self
+        """
         self.vars = '::~~::~~::'.join([self.name, self.username, self.email, self.password, self.pin])
         return self
 
     @staticmethod
     def get_key(password):
+        """
+        Gets the key needed to decrypt the stored data
+
+        :param password: str
+        :return: bytes
+        """
         password = password.encode()
         salt = b'*\x1e\xd5\\-\x99x\xc9\xc5{\xaa\xfa\xaa\n\x19\xf3'
         kdf = PBKDF2HMAC(
@@ -34,6 +55,11 @@ class Entry:
         return key
 
     def add(self):
+        """
+        Add more data to the txt.passwords, writes it in byte mode
+
+        :return: self
+        """
         secret = self.vars.encode()
 
         f = Fernet(self.get_key(self.big_pass))
@@ -43,24 +69,42 @@ class Entry:
             file.write(b'\n')
         return self
 
-    def get_list_of_entries(self):
-        file = open('dat//txt.passwords', 'rb')
-        encrypted = file.readlines()
-        for num, write in enumerate(encrypted):
-            f = Fernet(Entry.get_key(self.big_pass))
-            decrypt = f.decrypt(write).decode('utf-8')
-            decrypt = decrypt.split('::~~::~~::')
-            self.decrypt_list.append(decrypt)
-            print(f"{num}) {decrypt[0]}")
+    def print_entries(self):
+        """
+        Asks the user which entry they want to print, and prints it
+
+        :return: self
+        """
         choice = input('Which account would you like to view? ')
-        while not choice.isdigit():
+        while not choice.isdigit():  # only allow valid inputs
             choice = input('Which account would you like to view? ')
         choice = int(choice)
         account = self.decrypt_list[choice]
-        print(f'\n\nName of account:      {account[0]}')
+        print('\n\n')
+        print(f'Name of account:      {account[0]}')
         print(f'Username of account:  {account[1]}')
         print(f'Email for account:    {account[2]}')
         print(f'Password of account:  {account[3]}')
-        print(f'PIN for account:      {account[4]}\n\n')
+        print(f'PIN for account:      {account[4]}')
+        print('\n\n')
+        return self
+
+    def get_list_of_entries(self):
+        """
+        Accesses the data stored on txt.passwords, and sorts it to be printed
+
+        :return: self
+        """
+        file = open('dat//txt.passwords', 'rb')
+        encrypted = file.readlines()
+        for num, write in enumerate(encrypted):  # iterates through data to sort it
+            f = Fernet(Entry.get_key(self.big_pass))
+            decrypt = f.decrypt(write).decode('utf-8')
+            decrypt = decrypt.split('::~~::~~::')  # splits data by type
+            self.decrypt_list.append(decrypt)
+            print(f"{num}) {decrypt[0]}")
+        self.print_entries()  # prints data
+        return self
+
 
 
--- setup.py
+++ setup.py
@@ -7,23 +7,40 @@ from EntryClass import Entry
 h = hashlib.sha256()
 
 
-def check_pass(hashp):
+def check_pass(hashed_password):
+    """
+    Checks if the password is valid by comparing its hash to the stored one
+
+    :param hashed_password: bytes
+    :return: bool
+    """
     if os.path.exists('dat//txt.passwords') and os.path.exists('dat//passhash.txt'):
         with open('dat//passhash.txt', 'r') as f:
             pass_hash = f.read()
-        return hashp == pass_hash
+        return hashed_password == pass_hash
     else:
         tamper_proof()
 
 
 def add_space(string):
+    """
+    Adds a space to the string to make comprehension easier
+
+    :param string: str
+    :return: str
+    """
     if string == '':
         string += ' '
     return string
 
 
-
 def get_info(big_pass):
+    """
+    Has a dialog with the user to get the data that will be stored
+
+    :param big_pass: str
+    :return: Entry
+    """
     print("Just hit enter if you don't want to fill out the field")
     name = add_space(input('Name of account: '))
     username = add_space(input('Username of account: '))
@@ -34,6 +51,11 @@ def get_info(big_pass):
 
 
 def tamper_proof():
+    """
+    If the password file is wiped then it deletes all of the data
+
+    :return: None
+    """
     with open('dat//passhash.txt', 'w') as f:
         f.write('')
     with open('dat//txt.passwords', 'w') as f:
